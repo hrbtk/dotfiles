622a8ddffd79556d6ec2c5addcd3647d9d6f929f
hrbtk <viktor.beskorovainyi@gmail.com>
2024-06-21 21:04:19 +0200

    fix gnome script

--- fedora_after_install/5_gnome.sh
+++ fedora_after_install/5_gnome.sh
@@ -8,8 +8,8 @@ gext install caffeine@patapon.info
 gext install blur-my-shell@aunetx
 
 # Load keybindings to dconf
-dconf load /org/gnome/desktop/wm/keybindings/ < ./dconf_dumps/wmkbdump.dconf
-dconf load /org/gnome/shell/keybindings/ < ./dconf_dumps/shellkbdump.dconf
+dconf load /org/gnome/desktop/wm/keybindings/ < ~/dotfiles/fedora_after_install/dconf_dumps/wmkbdump.dconf
+dconf load /org/gnome/shell/keybindings/ < ~/dotfiles/fedora_after_install/dconf_dumps/shellkbdump.dconf
 
 # Copy wallpaopers
-cp ~/dotfiles/wallpapers ~/Pictures/
+cp -r ~/dotfiles/wallpapers ~/Pictures/
