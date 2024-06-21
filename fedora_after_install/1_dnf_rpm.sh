1a0bad565d38fd89609e142c045eacb3b102f118
hrbtk <viktor.beskorovainyi@gmail.com>
2024-06-21 20:46:16 +0200

    fix for dnf.conf

--- fedora_after_install/1_dnf_rpm.sh
+++ fedora_after_install/1_dnf_rpm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Modify dnf.conf
-sudo echo "fastestmirror=True 
+echo "fastestmirror=True 
 max_parallel_downloads=10 
 deltarpm=true
 defaultyes=True
-" >> /etc/dnf/dnf.conf
+" | sudo tee -a /etc/dnf/dnf.conf
 
 # Enable RPM Fusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
