3ecb3eb54df50a905a2367dc109465004ea23314
hrbtk <viktor.beskorovainyi@gmail.com>
2024-06-25 10:53:09 +0200

    some changes

--- fedora/3_software.sh
+++ fedora/3_software.sh
@@ -6,6 +6,9 @@ apps="git
 	eza
 	bat
 	fd-find
+	sd
+	jq
+	gh
 	ripgrep
 	afetch
 	btop
@@ -20,10 +23,6 @@ apps="git
 	adw-gtk3-theme"
 
 sudo dnf -y install $apps
-
-# Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
 
 # Setup flathub
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -50,17 +49,7 @@ sudo flatpak install -y flathub $fapps
 pipx install gnome-extensions-cli --system-site-packages
 pipx ensurepath
 
-# Install Google Chrome
-wget -O ~/Downloads/google-chrome-stable_current_x86_64.rpm  https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-sudo dnf install -y ~/Downloads/google-chrome-stable_current_x86_64.rpm
-
-# Install VS Code
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update
-sudo dnf install -y code
-
 # Copy config
-# cp -r ~/dotfiles/.config/alacritty ~/.config/
+cp -r ~/dotfiles/.config/alacritty ~/.config/
 cp ~/.bashrc ~/.bashrc.bak
 cp ~/dotfiles/.bashrc ~/.bashrc
---
+++ fedora/4_fonts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
+unzip Meslo.zip -d Meslo
+cp Meslo/*.ttf ~/.local/share/fonts
+rm -rf Meslo.zip Meslo
+
+wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
+unzip iafonts.zip -d iaFonts
+cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
+rm -rf iafonts.zip iaFonts
+
+fc-cache -f -v
+cd -
