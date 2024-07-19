93efafbb852645de1b23e24955a2f4b9e3088d31
hrbtk <viktor.beskorovainyi@gmail.com>
2024-07-19 19:05:40 +0200

    some changes

--- fedora/apps/app_yazi.sh
+++ fedora/apps/app_yazi.sh
@@ -6,12 +6,4 @@ unzip yazi-x86_64-unknown-linux-musl.zip -d yazi
 sudo install yazi/yazi /usr/local/bin
 rm yazi-x86_64-unknown-linux-musl.zip
 rm -rf yazi
-echo 'function yy() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
-}' > ~/.bashrc
 cd -
--- fedora/apps/app_zsh.sh
+++ fedora/apps/app_zsh.sh
@@ -5,7 +5,3 @@ sudo dnf -y install zsh
 
 # Change shell to zsh
 sudo chsh -s $(which zsh)
-
-# Copy config files
-cp ~/dotfiles/.zshrc ~/.zshrc
-cp -r ~/dotfiles/.config/zsh ~/.config/zsh
