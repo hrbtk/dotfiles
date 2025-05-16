ac34c9d889900d8623b86bc8d1b1c749eef2c02c
hrbtk <viktor.beskorovainyi@gmail.com>
2025-05-16 08:26:34 +0200

    some changes

---
+++ install/install_cli.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Select package manager
+if [[ -x "$(command -v dnf)" ]]; then
+  echo "Fedora"
+  INSTALLCOMM=$(sudo dnf install -y)
+elif [[ -x "$(command -v apt)" ]]; then
+  echo "Ubuntu"
+  INSTALLCOMM=$(sudo apt install -y)
+elif [[ -x "$(command -v yay)" ]]; then
+  echo "Arch Yay"
+  INSTALLCOMM=$(sudo yay -S --noconfirm)
+elif [[ -x "$(command -v pacman)" ]]; then
+  echo "Arch Pacman"
+  INSTALLCOMM=$(sudo pacman -S --noconfirm)
+else
+  echo "Unsupported"
+fi
+
+# List of packages
+PACKAGES="git 
+	fzf
+	micro 
+	eza
+	bat
+	fd-find
+	sd
+	jq
+	gh
+	ripgrep
+	btop
+	helix
+	unzip
+	ghostty
+	cargo"
+
+# Install packages
+echo("$INSTALLCOMM $PACKAGES")
