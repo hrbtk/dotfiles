462f609c2529e2ad20b5a2afef29fda4bb29d83b
hrbtk <viktor.beskorovainyi@gmail.com>
2025-08-01 13:35:34 +0200

    some changes + helix config

--- install/copy_config.sh
+++ install/copy_config.sh
@@ -1,48 +1,58 @@
 #!/bin/bash
 
-# Copy bash and zsh configs
+copy_config() {
+	# Copy bash and zsh configs
+	if [ -f "$HOME/.bashrc" ]; then
+		mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
+	fi
+	cp "$DEVDIR/dotfiles/.bashrc" "$HOME/.bashrc"
 
-if [ -f "$HOME/.bashrc" ]; then
-    mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
-fi
-cp "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
+	if [ -f "$HOME/.zshrc" ]; then
+		mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+	fi
+	cp "$DEVDIR/dotfiles/.zshrc" "$HOME/.zshrc"
 
-if [ -f "$HOME/.zshrc" ]; then
-    mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
-fi
-cp "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+	# bat
+	if [[ -x "$(command -v bat)" ]]; then
+		cp -r "$DEVDIR/dotfiles/.config/bat" "$HOME/.config"
+	fi
 
-# bat
-if [[ -x "$(command -v bat)" ]]; then
-    cp -r "$HOME/dotfiles/.config/bat" "$HOME/.config"
+	# ghostty
+	if [[ -x "$(command -v ghostty)" ]]; then
+		cp -r "$DEVDIR/dotfiles/.config/ghostty" "$HOME/.config"
+	fi
+
+	# yazi
+	if [[ -x "$(command -v yazi)" ]]; then
+		cp -r "$DEVDIR/dotfiles/.config/yazi" "$HOME/.config"
+	fi
+
+	# starship
+	if [[ -x "$(command -v starship)" ]]; then
+		cp -r "$DEVDIR/dotfiles/.config/starship" "$HOME/.config"
+	fi
+
+	# helix
+	if [[ -x "$(command -v helix)" ]] || [[ -x "$(command -v hx)" ]];  then
+		cp -r "$DEVDIR/dotfiles/.config/helix" "$HOME/.config"
+	fi
+
+	# kitty
+	if [[ -x "$(command -v kitty)" ]]; then
+		cp -r "$DEVDIR/dotfiles/.config/kitty" "$HOME/.config"
+	fi
+
+	# zed
+	if [[ -x "$(command -v zed)" ]]; then
+		cp -r "$DEVDIR/dotfiles/.config/zed" "$HOME/.config"
+	fi
+}
+
+
+if [[ -n $DEVDIR ]]; then
+	echo "DEVDIR variable exist"
+	copy_config
+else
+	echo "DEVDIR variable doesn't exist"
 fi
 
-# ghostty
-if [[ -x "$(command -v ghostty)" ]]; then
-    cp -r "$HOME/dotfiles/.config/ghostty" "$HOME/.config"
-fi
-
-# yazi
-if [[ -x "$(command -v yazi)" ]]; then
-    cp -r "$HOME/dotfiles/.config/yazi" "$HOME/.config"
-fi
-
-# starship
-if [[ -x "$(command -v starship)" ]]; then
-    cp -r "$HOME/dotfiles/.config/starship" "$HOME/.config"
-fi
-
-# helix
-if [[ -x "$(command -v helix)" ]] || [[ -x "$(command -v hx)" ]];  then
-    cp -r "$HOME/dotfiles/.config/helix" "$HOME/.config"
-fi
-
-# kitty
-if [[ -x "$(command -v kitty)" ]]; then
-    cp -r "$HOME/dotfiles/.config/kitty" "$HOME/.config"
-fi
-
-# zed
-if [[ -x "$(command -v zed)" ]]; then
-    cp -r "$HOME/dotfiles/.config/zed" "$HOME/.config"
-fi
--- install/install_fonts.sh
+++ install/install_fonts.sh
@@ -2,21 +2,21 @@
 
 [ ! -d "$HOME/.local/share/fonts" ] && mkdir -p "$HOME/.local/share/fonts"
 
-cd /tmp
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip
-unzip Meslo.zip -d Meslo
-cp -r Meslo "$HOME/.local/share/fonts/"
-rm -rf Meslo.zip Meslo
+DOWNLOADDIR=/tmp
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
-unzip FiraCode.zip -d FiraCode
-cp -r FiraCode "$HOME/.local/share/fonts/"
-rm -rf FiraCode.zip FiraCode
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip -P $DOWNLOADDIR 
+unzip $DOWNLOADDIR/Meslo.zip -d $DOWNLOADDIR/Meslo
+cp -r $DOWNLOADDIR/Meslo "$HOME/.local/share/fonts/"
+rm -rf $DOWNLOADDIR/Meslo.zip $DOWNLOADDIR/Meslo
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
-unzip JetBrainsMono.zip -d JetBrainsMono
-cp -r JetBrainsMono "$HOME/.local/share/fonts/"
-rm -rf JetBrainsMono.zip JetBrainsMono
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip -P $DOWNLOADDIR
+unzip $DOWNLOADDIR/FiraCode.zip -d $DOWNLOADDIR/FiraCode
+cp -r $DOWNLOADDIR/FiraCode "$HOME/.local/share/fonts/"
+rm -rf $DOWNLOADDIR/FiraCode.zip $DOWNLOADDIR/FiraCode
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip -P $DOWNLOADDIR
+unzip $DOWNLOADDIR/JetBrainsMono.zip -d $DOWNLOADDIR/JetBrainsMono
+cp -r $DOWNLOADDIR/JetBrainsMono "$HOME/.local/share/fonts/"
+rm -rf $DOWNLOADDIR/JetBrainsMono.zip $DOWNLOADDIR/JetBrainsMono
 
 fc-cache -f -v
-cd -
